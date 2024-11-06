1715decc9ebd868398aa5f7bc913a9078c7c4386
Pongskaorn Eingson <pongsaen@ais.co.th>
2024-11-06 09:00:12 +0000

    Initial

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/',(req,res) => {
 
-        res.send('Hello World - 4.0.0');
+        res.send('Hello World - 9.0.0');
 
 });
 app.listen(PORT,HOST,() => {
